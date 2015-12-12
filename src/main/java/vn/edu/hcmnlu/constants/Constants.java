22b12095ada0f043e77dbc7a85429e46048bd265
codocntt <codocntt@gmail.com>
2015-12-12 15:46:06 +0700

    aaaaaa

---
+++ src/main/java/vn/edu/hcmnlu/constants/Constants.java
@@ -0,0 +1,16 @@
+package vn.edu.hcmnlu.constants;
+
+public class Constants {
+	
+	public static final String TYPE = "doc";
+	public static final String INDICES = "com.edu.hcmnlu";
+
+	public static final String CLUSTER_KEY = "cluster.name";
+	//public static final String CLUSTER_NAME = "elasticsearch";
+
+	public static final String HOST_NAME = "localhost";
+	//public static final int HOST_PORT = 9300;
+	
+	public static final String RESOURCE_PATH = "data";
+
+}
