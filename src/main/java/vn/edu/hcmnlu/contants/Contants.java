a1d8dc768f972a4b1db094c89ca6e6111fdf03b1
codocntt <codocntt@gmail.com>
2015-12-12 15:35:14 +0700

    delete file constant

--- src/main/java/vn/edu/hcmnlu/contants/Contants.java
+++
@@ -1,9 +0,0 @@
-package vn.edu.hcmnlu.contants;
-
-
-public class Contants {
-	public static final String TYPE = "doc";
-	public static final String INDICES = "nlu";
-	public static final String RESOURCE_PATH = "data";
-	
-}
